04d146d57e4525d27abf401f40ed1eca8e79c289
emiliocraft23 <48366274+emiliocraft23@users.noreply.github.com>
2026-05-07 15:00:28 -0500

    Update xor_cipher.py

--- xor_cipher.py
+++ xor_cipher.py
@@ -1,11 +1,12 @@
 def xor(t, k):
     resultado = ""
-    for i, c in enumerate(t):
-        bit_llave = int(k[i % len(k)])
-        resultado += chr(ord(c) ^ bit_llave)
+    for i in range(0, len(t), 8):
+        bloque = t[i:i+8]
+        for j, c in enumerate(bloque):
+            bit_llave = int(k[j])
+            resultado += chr(ord(c) ^ bit_llave) 
     return resultado
-
-t = "Hola Mundo!"
+t = "Hola mundo!"
 k = "11001010"
 c = xor(t, k)
 print(f"Original: {t}\nLlave: {k}\nCifrado: {repr(c)}\nDescifrado: {xor(c, k)}")
